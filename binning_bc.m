93113d1e721eb5b139f21de204eeb7a99ed2522c
UG\ogeorgi <ogeorgi@kth.se>
2019-02-15 14:40:43 +0100

    Added first version of binning correction algorithm from Nickolay

---
+++ binning_bc.m
@@ -0,0 +1,55 @@
+function [n_read, n_coadd] = binning_bc(Ncol,Ncolskip,NcolbinFPGA,NcolbinCCD,BadColumns);
+
+%
+% a routine to estimate the correction factors for column binning with
+% bad columns
+%
+% n_read  - array, containing the number of individually read superpixels
+%           contributing to the given superpixel
+% n_coadd - array, containing the number of co-added individual pixels
+%
+% Input - as per ICD. BadColumns - array containing the index of bad
+%           columns (the index of first column is 0)
+%
+
+n_read =1:Ncol+1;
+n_coadd=1:Ncol+1;
+
+if NcolbinFPGA==0 
+    NcolbinFPGA_=1;
+else
+    NcolbinFPGA_=NcolbinFPGA;
+end
+
+if NcolbinCCD==0 
+    NcolbinCCD_=1;
+else
+    NcolbinCCD_=NcolbinCCD;
+end
+
+col_index=Ncolskip;
+
+n_read(:)=0;
+n_coadd(:)=0;
+
+for j_col=1:Ncol+1 
+    for j_FPGA=1:NcolbinFPGA_
+        continuous=0;
+        for j_CCD=1:NcolbinCCD_
+            if ismember(col_index, BadColumns)
+                if (continuous == 1)
+                    n_read(j_col)=n_read(j_col)+1;
+                end
+                continuous=0;
+            else
+                continuous = 1;
+                n_coadd(j_col)=n_coadd(j_col)+1;
+            end
+            
+            col_index=col_index + 1;
+        end
+        if (continuous == 1) 
+            n_read(j_col)=n_read(j_col)+1;
+        end
+    end
+end
