4cce8aa8fb221fd676c66c1a88251374c8e4db7a
UG\ogeorgi <ogeorgi@kth.se>
2019-02-19 11:19:18 +0100

    More computation improvement - gain change only for the pixels with bad columns

--- compensate_bad_columns.m
+++ compensate_bad_columns.m
@@ -29,13 +29,11 @@ end;
 [n_read, n_coadd] = binning_bc(ncol,ncolskip,ncolbinF,ncolbinC,header.BadCol);
 
 if ncolbinC > 1
-    
-    % Remove gain
-    image = image*gain;
-
     for j_c=1:ncol
-        
         if (ncolbinC*ncolbinF ~= n_coadd(j_c))
+            % Remove gain adjustment
+            image(1:nrow,j_c)= image(1:nrow,j_c)*gain;
+            
             % Remove added superpixel value due to bad columns and readout offset
             image(1:nrow,j_c)= image(1:nrow,j_c) - n_read(j_c)*(blank-128) - 128;
 
@@ -45,12 +43,12 @@ if ncolbinC > 1
             % Add number of FPGA binned
             image(1:nrow,j_c)=image(1:nrow,j_c) + ncolbinF*(blank-128) + 128;
             
+            % Add gain adjustment back
+            image(1:nrow,j_c)= image(1:nrow,j_c)/gain;
+            
             fprintf('Col: %d, n_read: %d, n_coadd: %d, binned pixels: %d\n', j_c, n_read(j_c), n_coadd(j_c), ncolbinC*ncolbinF); 
         end
     end
-
-    image = image/gain;
-
 end
 
 end
