9d412c25e27fa7c4d20bede29f688e2b62c34f4c
UG\ogeorgi <ogeorgi@kth.se>
2019-03-01 17:15:38 +0100

    Fixed issue with true image calculation if CCD binning is 0

--- get_true_image.m
+++ get_true_image.m
@@ -3,11 +3,16 @@ function [true_image] = get_true_image (image, header)
 % Calculate true image by removing readout offset, pixel blank value and
 % normalizaing the signal level according to readout time
 
+ncolbinC=header.NColBinCCD;
+if (ncolbinC == 0)
+    ncolbinC = 1;
+end
+
 % Remove gain
 true_image = image * 2^bitand(header.Gain,255);
 
 % Bad column analysis
-[n_read, n_coadd] = binning_bc(header.NCol + 1,header.NColSkip,2^header.NColBinFPGA,header.NColBinCCD,header.BadCol);
+[n_read, n_coadd] = binning_bc(header.NCol + 1,header.NColSkip,2^header.NColBinFPGA,ncolbinC,header.BadCol);
 
 % Go throught the columns
 for j_c=1:header.NCol + 1
@@ -15,7 +20,7 @@ for j_c=1:header.NCol + 1
     true_image(1:header.NRow,j_c)= true_image(1:header.NRow,j_c) - n_read(j_c)*(header.BlankTrailingValue-128) - 128;
     
     % Compensate for bad columns
-    true_image(1:header.NRow,j_c)= true_image(1:header.NRow,j_c) * (2^header.NColBinFPGA*header.NColBinCCD/n_coadd(j_c));
+    true_image(1:header.NRow,j_c)= true_image(1:header.NRow,j_c) * (2^header.NColBinFPGA*ncolbinC/n_coadd(j_c));
 end
 
 end
